f3a281dc0f079fd7ae4c5cea9207f5b45e4c3fd5
corvinusy <mz5corvinus@gmail.com>
2013-12-14 20:40:04 +0400

    118

---
+++ euler/euler179/euler179.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+    "fmt"
+	"github.com/cznic/mathutil"
+)
+
+const LIMIT = 1e7
+
+func main() {
+
+	var (
+		dLen, prevDivLen, i int
+	)
+	
+	count := 0
+
+	prevDivLen = divLen(2)
+
+	for i = 3; i < LIMIT; i, prevDivLen = i + 1, dLen {
+
+		dLen = int(divLen32(uint32(i)))
+		
+		if dLen == prevDivLen {
+			count++
+		} 
+
+	}
+
+	fmt.Println(count)
+	
+}
+/*----------------------------------------------------------------------------*/
+func divLen32(n uint32) uint32 {
+
+	result := uint32(1)
+
+	fts := mathutil.FactorInt(n)
+
+	for _, ft := range fts {
+		result *= (ft.Power + 1)
+	}
+
+	return result
+}
+/*----------------------------------------------------------------------------*/
+func divLen(n int) int {
+
+	var i, count int
+
+	count = 0
+
+	for i = 1; i <= n; i++ {
+		if n % i == 0 {
+			count += 1
+		}
+	}
+
+	return count
+}
+/*----------------------------------------------------------------------------*/
---
+++ euler/euler265/euler265.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "fmt"
+	"github.com/cznic/mathutil"
+)
+
+const LIMIT = 1e8
+
+func main() {
+
+	var ftslen int
+
+	count := 0
+
+	for i := 4; i < LIMIT; i++ {
+
+		fts := mathutil.FactorInt(uint32(i))
+		ftslen = len(fts)
+
+		switch ftslen {
+		case 2 :
+			if fts[0].Power == 1 && fts[1].Power == 1 {
+				count ++ 
+			}
+
+		case 1 :
+			if fts[0].Power == 2 { 
+				count++
+			}
+		}
+
+	}
+
+	fmt.Println(count)
+	
+}
