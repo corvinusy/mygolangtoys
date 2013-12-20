d92fb14fc48d025c4235d0bca81b4c76b82e486b
corvinusy <mz5corvinus@gmail.com>
2013-12-20 05:41:45 +0400

    123

--- euler/euler084/euler084.go
+++ euler/euler084/euler084.go
@@ -2,46 +2,146 @@ package main
 
 import (
     "fmt"
+    "time"
+    "math/rand"
 )
 
-const LIMIT = 6000 // maxlen 
+const (
+    LIMIT = 1e7 // rounds
+    GOUT = 0
+    JAIL = 10
+    G2J = 30
+    CC1 = 2
+    CC2 = 17
+    CC3 = 33
+    CH1 = 7
+    CH2 = 22
+    CH3 = 36
+    NEXTR = -1
+    R1 = 5
+    R2 = 15
+    R3 = 25
+    R4 = 35
+    NEXTU = -2
+    U1 = 12
+    U2 = 28
+    MINUS3 = -3
+    STAND = -5
+)
 
 func main() {
 
-	cache := prepare_cache() //cache = map of squares
+    r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	var l, c int
+    var a [40]int
+    var dbls int
+    
+    p := 0
+    a[0] = 1
 
-	count := 0
+    for i := 0; i < LIMIT; i++ {
+        d, dbl := rollDices(r)
 
-	for size := 2; ; size++ {
+        if dbl {
+            dbls += 1
+        } else {
+            dbls = 0
+        }
 
-		for a := 1; a <= size; a++ {
-			for b := a; b <= size; b++ {
-				c = size
+        if dbls == 3 {
+            p = JAIL
+            d = 0
+            dbls = 0
+        }
 
-				l = c * c + (b+a) * (b+a) // if a <= b <= c
+        p = (p + d) % 40
+        switch p {
+        case G2J : p = 10
+        case CH1, CH2, CH3 : { 
+            switch rollCH(r) {
+            case GOUT : p = GOUT
+            case JAIL : p = JAIL
+            case R1 : p = R1
+            case 11 : p = 11
+            case 24 : p = 24
+            case 39 : p = 39
+            case NEXTR: {
+                switch {
+                case p < R1 : p = R1
+                case p < R2 : p = R2
+                case p < R3 : p = R3
+                case p < R4 : p = R4
+                default : p = R1
+                }
+            } 
+            case NEXTU: {
+                switch {
+                case p < U1 : p = U1
+                case p < U2 : p = U2
+                default : p = U1
+                }               
+            }
 
-				if cache[l] {
-					count++
-				}
-			}
-		}
+            case MINUS3: p = (p + 37) % 40
+            }
+        }
+        case CC1, CC2, CC3 : {
+            switch rollCC(r) {
+            case JAIL : p = JAIL
+            case GOUT : p = GOUT
+            }
+        }
+            
+        }
 
-//		fmt.Println("size =", size, "count=", count)
-		if count > 1e6 {
-			fmt.Println("result = ", size)
-			return
-		}
-	}
-	
+        a[p] += 1
+    }
+
+    for i := range a {
+        if a[i] > 3e5 {
+            fmt.Printf("%2d:%d \n", i, a[i])
+        }
+    }
+
 }
-/*-----------------------------------------------------------------------------*/
-func prepare_cache() map[int]bool {
+/*----------------------------------------------------------------------------*/
+func rollDices(r *rand.Rand) (int, bool) {
+    
+    d1 := r.Intn(4)
+    d2 := r.Intn(4)
+    return 2 + d1 + d2, d1 == d2
+    
+}
+/*----------------------------------------------------------------------------*/
+func rollCC(r *rand.Rand) int {
 
-	cache := make(map[int]bool, 0)
-	for i := 1; i <= LIMIT; i++ {
-		cache[i*i] = true
-	}
-	return cache
+    d := r.Intn(16)
+
+    if d == 1 {
+        return JAIL
+    }
+
+    if d == 2 {
+        return GOUT
+    }
+
+    return STAND
+}
+/*----------------------------------------------------------------------------*/
+func rollCH(r *rand.Rand) int {
+
+    d := r.Intn(16)
+
+    switch d  {
+    case 0: return GOUT
+    case 1: return JAIL
+    case 2: return 11
+    case 3: return 24
+    case 4: return 39
+    case 5: return 5
+    case 6, 7: return NEXTR
+    case 8: return NEXTU
+    case 9: return MINUS3
+    default: return STAND
+    }
 }
--- euler/euler088/euler088.go
+++ euler/euler088/euler088.go
@@ -2,115 +2,46 @@ package main
 
 import (
     "fmt"
+	"github.com/cznic/mathutil"
 )
+
+const LIMIT = 49
 
 func main() {
 
-	for k := 7; k <= 7; k++ {
-		vec := make([]int, k)
-		for i, _ := range vec {
-			vec[i] = 1
+	var i, sum, ss, count uint32
+
+	bigsum := 1
+
+	summap := make(map[uint32]uint32)
+
+	summap[1] = 1
+
+	for i = 2; i <= LIMIT*2; i++ {
+
+		fts := mathutil.FactorInt(uint32(i))
+
+		ss = 0
+		count = 0
+
+		for _, ft := range fts {
+			ss += ft.Prime * ft.Power //error!
+			count += ft.Power
 		}
-		
-		for /*vsum(vec) != vproduct(vec)*/ {
-			inc(vec)
-			fmt.Println(vec, vsum(vec), vproduct(vec))
-		}
-		
-		fmt.Println("result", k, vec, vsum(vec))
-	}
-}
-/*-----------------------------------------------------------------------------*/
-func vsum(vec []int) int {
-	sum := 0
-	for i, _ := range vec {
-		sum += vec[i]
-	}
-	return sum
-}
-/*-----------------------------------------------------------------------------*/
-func vproduct(vec []int) int {
-	prod := 1
-	for i, _ := range vec {
-		prod *= vec[i]
-	}
-	return prod
-}
-/*-----------------------------------------------------------------------------*/
-func inc(vec []int) {
 
-	lim := len(vec)
-
-	i := lim-1
-
-	for ;i > 0; i-- {
-		if vec[i] < vec[i-1] {
-			vec[i]++
-			if i == lim - 1 {
-				break
+		for j := uint32(len(fts)); j <= count; j++ {
+			sum = i - ss + j
+			if sum <= LIMIT && summap[sum] == 0 {
+				summap[sum] = i
 			}
-			for j := i+1; j < lim; j++ {
-				vec[j] = 1
-			}
-
-			return
 		}
 	}
 
-	vec[0]++
-
-	if vec[0] > lim {
-		panic("fuck")
+	for _, v := range summap {
+		bigsum += int(v)
 	}
 
-	for j := 1; j < lim; j++ {
-		vec[j] = 1
-	}
+	fmt.Println(bigsum, summap)
 
-	return
 }
 /*-----------------------------------------------------------------------------*/
-func divsum(num int) int {
-
-	var (
-		sum int = 1
-		acc int = 1
-		tmp, i int
-	)
-	
-	//trivial case
-	if num == 1 {
-		return num
-	}
-	// powers of 2
-	for num % 2 == 0 {
-		acc *= 2
-		num /= 2
-	}
-	acc = acc * 2 - 1
-	
-	if num == 1 {
-		return acc
-	} else {
-		sum = acc
-	}
-	
-	// brute force others
-	for i = 3; i * i <= num; i += 2 {
-		acc = 1
-		tmp = 1
-		for num % i == 0 {
-			acc = acc * i
-			tmp = tmp + acc
-			num = num / i
-		}
-		if (acc > 1) {
-			sum = sum * tmp
-		}
-	}
-
-	if (num > 1) {
-		sum = sum * (num + 1)
-	}
-	return sum;
-}
