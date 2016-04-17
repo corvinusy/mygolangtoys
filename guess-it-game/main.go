e215b34a18bbe9ba5624d0ccb6bd1a65f3a471d6
corvinusy <mz5corvinus@gmail.com>
2016-04-17 21:03:10 +0300

    added guess-it-game example

---
+++ guess-it-game/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+var (
+	pattern = "Hello my Golang"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	inout := make(chan string) // data transferring between
+	// start reading input routine
+	fmt.Print("Enter text ('quit' to stop): ")
+	go readInput(reader, inout)
+	// start processing input routine
+	go processInput(inout)
+	// wait proc
+	time.Sleep(1 * time.Hour)
+}
+
+//------------
+func readInput(reader *bufio.Reader, out chan string) {
+	for {
+		input, _ := reader.ReadString('\n')
+		//fmt.Printf("%+q", input)
+		if input == "quit\n" {
+			os.Exit(0)
+		}
+	}
+}
+
+//------------
+func processInput(in chan string) {
+	for line := range in {
+		total := 0
+		for i := range line {
+			if line[i] == pattern[i] {
+				total++
+			}
+		}
+		fmt.Println(total)
+	}
+}
