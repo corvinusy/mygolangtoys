e91d3de13aec79039d3bfb2a9f576127cce9280c
corvinusy <mz5corvinus@gmail.com>
2016-11-09 15:36:02 +0300

    updated hourglass

--- hackerrank/hourglass/main.go
+++ hackerrank/hourglass/main.go
@@ -19,8 +19,8 @@ func main() {
 }
 
 func getAllSum(x [][]int) int {
-	max := 0
-	sum := 0
+	var sum int
+	max := getHgSum(x, 0, 0)
 	for i := 0; i < size-hgSize+1; i++ {
 		for j := 0; j < size-hgSize+1; j++ {
 			sum = getHgSum(x, i, j)
@@ -33,13 +33,12 @@ func getAllSum(x [][]int) int {
 }
 
 func getHgSum(x [][]int, r, c int) int {
-	sum := 0
+	var sum int
 	for i := r; i < r+hgSize; i++ {
 		for j := c; j < c+hgSize; j++ {
 			sum += x[i][j]
 		}
 	}
 	sum = sum - x[r+1][c] - x[r+1][c+2]
-	fmt.Println(sum)
 	return sum
 }
