16be6e4bd190a7d8ab06421508af25fa2fafb5cc
corvinusy <mz5corvinus@gmail.com>
2016-11-18 20:33:52 +0300

    hr: add repeated-string

---
+++ hackerrank/repeated-string/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var n int
+	rd := bufio.NewReader(os.Stdin)
+	s, _ := rd.ReadBytes('\n')
+	s = s[:len(s)-1] // trimRight '\r'
+	ns, _ := rd.ReadString('\n')
+	fmt.Sscan(ns, &n)
+
+	a := countA(s)
+	if a == 0 {
+		fmt.Println(0)
+		return
+	}
+
+	rem := s[:n%len(s)]
+	as := a*(n/len(s)) + countA(rem)
+	fmt.Println(as)
+}
+
+func countA(s []byte) int {
+	var count int
+	for i := range s {
+		if s[i] == 97 {
+			count++
+		}
+	}
+	return count
+}
