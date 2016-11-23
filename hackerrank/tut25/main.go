873c0ff85e0535a462f07c8c45dd178a4078524e
corvinusy <mz5corvinus@gmail.com>
2016-11-24 00:39:11 +0300

    hr: add tut25

---
+++ hackerrank/tut25/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+var (
+	sieve    map[int]bool
+	sieveMax int
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	data := make([]int, n)
+	for i := range data {
+		fmt.Scan(&data[i])
+	}
+
+	sieve = make(map[int]bool)
+	sieve[2] = true
+	sieve[3] = true
+	sieve[5] = true
+	sieve[7] = true
+
+	sieveMax = 7
+
+	for i := range data {
+		if isPrime(data[i]) {
+			fmt.Println("Prime")
+		} else {
+			fmt.Println("Not prime")
+		}
+	}
+}
+
+func isPrime(x int) bool {
+	switch {
+	case x == 1:
+		return false
+	case x == 2:
+		return true
+	case x%2 == 0:
+		return false
+	default:
+		sqr := int(math.Sqrt(float64(x))) + 1
+		extendSieve(sqr)
+		fmt.Println(sieve)
+		return !isDivisable(x)
+	}
+}
+
+func extendSieve(cap int) {
+	var ok bool
+	for i := sieveMax + 2; sieveMax <= cap; i += 2 {
+		ok = true
+		for k := range sieve {
+			if i%k == 0 {
+				ok = false
+				break
+			}
+		}
+		if ok {
+			sieve[i] = true
+			sieveMax = i
+		}
+
+	}
+}
+
+func isDivisable(x int) bool {
+	if sieve[x] {
+		return false
+	}
+	for k := range sieve {
+		if x%k == 0 {
+			return true
+		}
+	}
+	return false
+}
