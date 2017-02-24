b2c1eeab9fed86363de6fab0b1e6c2d0b411493d
corvinusy <mz5corvinus@gmail.com>
2017-02-24 22:01:51 +0300

    update bot-clean-partially-observable (incompleted)

--- hackerrank/machine-learning/bot-clean-partially-observable/main.go
+++ hackerrank/machine-learning/bot-clean-partially-observable/main.go
@@ -6,7 +6,8 @@ import (
 	"os"
 )
 
-type theBot struct {
+// Agent ...
+type Agent struct {
 	pos   cell
 	board []string
 }
@@ -15,125 +16,94 @@ type cell struct {
 	x, y int
 }
 
-type quadrant struct {
-	xmin, xmax, ymin, ymax int
-}
-
 const boardSize = 5
 
+var mainLine = []cell{{0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}}
+
 func main() {
+	bot := new(Agent)
+	bot.getWorldState()
+	println(bot.nextMove())
+}
+
+func (a *Agent) getWorldState() error {
 	var (
+		err error
 		s   string
-		bot theBot
 	)
 	rd := bufio.NewReader(os.Stdin)
-	s, _ = rd.ReadString('\n')
-	fmt.Sscanf(s, "%d %d", &bot.pos.x, &bot.pos.y)
-	bot.board = make([]string, boardSize)
-	for i := range bot.board {
-		s, _ = rd.ReadString('\n')
-		bot.board[i] = s[:boardSize]
+	s, err = rd.ReadString('\n')
+	if err != nil {
+		return err
 	}
-	bot.nextMove()
+	fmt.Sscanf(s, "%d %d", &a.pos.x, &a.pos.y)
+	a.board = make([]string, boardSize)
+	for i := range a.board {
+		s, err = rd.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		a.board[i] = s[:boardSize]
+	}
+	return nil
 }
 
-func (bot *theBot) nextMove() {
-	// find quadrant
-	q := bot.findQuadrant()
-	// search dirty cell
-	c, found := bot.findDirtyCell(q)
-	if !found {
-		c = bot.moveToNextQuadrant()
+func (a *Agent) nextMove() string {
+	// if isDirty then CLEAN
+	if a.isOnDirtyCell() {
+		return "CLEAN"
 	}
-	direction := bot.moveToCell(c)
-	fmt.Println(direction)
+	// else
+	// if not on MainLine then move to closest Main Line
+	if !a.isOnMainLine() {
+		return a.moveTo(a.closestMainLine())
+	}
+	// if on MainLine then find dirty
+	if dirtyCell, found := a.findDirtyCell(); found {
+		return a.moveTo(dirtyCell)
+	}
+	return ""
 }
 
-func (bot *theBot) findQuadrant() quadrant {
-	switch {
-	case bot.pos.x < 3 && bot.pos.y < 3:
-		return quadrant{0, 2, 0, 2}
-	case bot.pos.x < 3 && bot.pos.y >= 3:
-		return quadrant{0, 2, 3, 4}
-	case bot.pos.x >= 3 && bot.pos.y < 3:
-		return quadrant{3, 4, 0, 2}
-	case bot.pos.x >= 3 && bot.pos.y >= 3:
-		return quadrant{3, 4, 3, 4}
-	}
-	panic("out of board")
+func (a *Agent) isOnDirtyCell() bool {
+	return a.board[a.pos.x][a.pos.y] == 'd'
 }
 
-func (bot *theBot) findDirtyCell(q quadrant) (cell, bool) {
-	var (
-		x, y int
-		c    cell
-	)
-	// at first look current cell
-	c = cell{bot.pos.x, bot.pos.y}
-	if bot.checkCell(c, 'd') {
-		return c, true
-	}
-	// look cell to left and right
-	for y = bot.pos.y - 1; y >= bot.pos.y+1; y += 2 {
-		c = cell{bot.pos.x, y}
-		if bot.checkQuadrant(q, c) && bot.checkCell(c, 'd') {
-			return c, true
+func (a *Agent) isOnMainLine() bool {
+	for i := range mainLine {
+		if a.pos == mainLine[i] {
+			return true
 		}
 	}
-	// look at cell up and down
-	for x = bot.pos.x - 1; x <= bot.pos.x+1; x += 2 {
-		c = cell{x, bot.pos.y}
-		if bot.checkQuadrant(q, c) && bot.checkCell(c, 'd') {
-			return c, true
-		}
-	}
+	return false
+}
 
-	// look cells at diags
-	for x = bot.pos.x - 1; x <= bot.pos.x+1; x += 2 {
-		for y = bot.pos.y + 1; y >= bot.pos.y-1; y -= 2 {
-			c = cell{x, y}
-			if bot.checkQuadrant(q, c) && bot.checkCell(c, 'd') {
-				return c, true
-			}
-		}
-	}
+func (a *Agent) findDirtyCell() (cell, bool) {
 	return cell{}, false
 }
 
-func (bot *theBot) moveToCell(c cell) string {
+func (a *Agent) closestMainLine() cell {
+	switch a.pos.x {
+	case 0:
+		return cell{1, a.pos.y}
+	case 2, 4:
+		return cell{3, a.pos.y}
+	default:
+		return cell{a.pos.x, a.pos.y}
+	}
+}
+
+func (a *Agent) moveTo(c cell) string {
 	switch {
-	case bot.pos.y < c.y:
-		return "RIGHT"
-	case bot.pos.y > c.y:
-		return "LEFT"
-	case bot.pos.x > c.x:
+	case a.pos.x > c.x:
 		return "UP"
-	case bot.pos.x < c.x:
+	case a.pos.x < c.x:
 		return "DOWN"
+	case a.pos.y < c.y:
+		return "RIGHT"
+	case a.pos.y > c.y:
+		return "LEFT"
 	default:
 		return "CLEAN"
 	}
-}
-
-func (bot *theBot) moveToNextQuadrant() cell {
-	switch {
-	case bot.pos.x < 3 && bot.pos.y < 3:
-		if bot.pos.x != 1 {
-			return cell{1, 1}
-		}
-		return cell{1, 3}
-	case bot.pos.x < 3 && bot.pos.y >= 3:
-		return cell{3, 3}
-	case bot.pos.x >= 3 && bot.pos.y >= 3:
-		return cell{3, 1}
-	}
-	return cell{1, 1}
-}
-
-func (bot *theBot) checkQuadrant(q quadrant, c cell) bool {
-	return c.x >= q.xmin && c.x <= q.xmax && c.y >= q.ymin && c.y <= q.ymax
-}
-
-func (bot *theBot) checkCell(c cell, v byte) bool {
-	return bot.board[c.x][c.y] == v
 }
