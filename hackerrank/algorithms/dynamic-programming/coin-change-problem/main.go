0ab3b9694ca375856cdb53300844d6346f33bf0c
corvinusy <mz5corvinus@gmail.com>
2017-08-20 20:33:38 +0300

    hr: add coin-exchange

--- hackerrank/algorithms/dynamic-programming/coin-change-problem/main.go
+++ hackerrank/algorithms/dynamic-programming/coin-change-problem/main.go
@@ -5,69 +5,25 @@ import (
 	"sort"
 )
 
-func panicIfErr(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 func main() {
-	var (
-		err  error
-		n, r int
-	)
-	_, err = fmt.Scan(&n, &r)
-	panicIfErr(err)
-	ms := make([]int, r)
-	for i := range ms {
-		_, err = fmt.Scan(&ms[i])
-		panicIfErr(err)
+	var sum, n int
+	fmt.Scan(&sum, &n)
+	coins := make([]int, n)
+	for i := range coins {
+		fmt.Scan(&coins[i])
 	}
-	sort.Ints(ms)
-	fmt.Println(remains(n, ms))
+	sort.Ints(coins)
+	result := countWays(coins, sum)
+	fmt.Println(result)
 }
 
-func remains(n int, ms []int) int {
-	var (
-		ok bool
-	)
-	p := map[int]map[int]bool{}
-	for i := 0; i <= n; i++ {
-		p[n] = make(map[int]bool)
-	}
-	m := map[int]int{}
-	m[0] = 0
-	p[ms[0]][1] = true
-	for i := 1; i <= n; i++ {
-		if _, ok = m[i]; !ok {
-			getRemains(i, ms, m, p)
+func countWays(coins []int, sum int) int {
+	ways := make([]int, sum+1)
+	ways[0] = 1
+	for i := range coins {
+		for j := coins[i]; j <= sum; j++ {
+			ways[j] += ways[j-coins[i]]
 		}
 	}
-	fmt.Printf("%+v\n, %+v\n", m, p)
-	return m[n]
-}
-
-func getRemains(n int, ms []int, m map[int]int, p map[int]map[int]bool) {
-	var (
-		ok bool
-		k  int
-	)
-	m[n] = 0
-	p[n] = make(map[int]bool)
-	for _, v := range ms {
-		if n-v < 0 {
-			continue
-		}
-		if n-v == 0 && !p[n][k] {
-			m[n]++
-			k++
-			p[n][k] = true
-		}
-		if _, ok = m[n-v]; !ok {
-			getRemains(n-v, ms, m, p)
-		}
-		m[n] += m[n-v]
-		k = k << 8
-	}
-
+	return ways[sum]
 }
